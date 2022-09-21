cec3acf51a5bba3823c16be7512920311a771f5f
Murata Ugetsu <92856337+ugetsu-light@users.noreply.github.com>
2022-09-21 09:22:54 +0800

    started jwt

--- customers/views.py
+++ customers/views.py
@@ -1,10 +1,13 @@
 from customers.models import Customer
 from customers.serializer import CustomerSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def customers(request):
     """invoke serializer and return to client"""
     if request.method == 'GET':
@@ -19,8 +22,8 @@ def customers(request):
             return Response({'customer':serializer.data}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 @api_view(['GET', 'POST', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def customer(request, id):
     """individual customer"""
     try:
