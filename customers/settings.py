75dd70491aff159622cd31e8e71898a77bd880e5
Murata Ugetsu <92856337+ugetsu-light@users.noreply.github.com>
2022-09-25 15:32:54 +0800

    refresh tokens

--- customers/settings.py
+++ customers/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ROTATE_REFRESH_TOKENS': True,
+}
 
 # Application definition
 
--- customers/urls.py
+++ customers/urls.py
@@ -19,8 +19,8 @@ from customers import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/', TokenRefreshView.as_view(), name='token_obtain_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api/customers/', views.customers, name='customers'),
     path('api/customers/<int:id>', views.customer, name='customer')
